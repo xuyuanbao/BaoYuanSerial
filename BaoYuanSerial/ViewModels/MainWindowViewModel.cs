bda37b87a4e5922ce76a27bc72cc42618eb85bb2
xuyuanbao <416315797@qq.com>
2021-06-07 23:19:07 +0800

    Amend MainWindow
    Amend MainWindow and MainWindowViewModel

--- BaoYuanSerial/ViewModels/MainWindowViewModel.cs
+++ BaoYuanSerial/ViewModels/MainWindowViewModel.cs
@@ -9,6 +9,7 @@ using MessageBox.Avalonia.DTO;
 using MessageBox.Avalonia.Enums;
 using System.Collections.ObjectModel;
 using BaoYuanSerial.Services;
+using Avalonia.Media;
 
 namespace BaoYuanSerial.ViewModels
 {
@@ -19,6 +20,8 @@ namespace BaoYuanSerial.ViewModels
         {
             SerialPortList = SerialService.GetSerials();
             DataBitsIndex = 3;
+            _ComPortState = SerialPortList[PortNameIndex] + " ClOSED";
+
         }
 
         private ObservableCollection<string> _SerialPortList = new ObservableCollection<string>();
@@ -44,6 +47,9 @@ namespace BaoYuanSerial.ViewModels
                 this.RaisePropertyChanged();
             }
         }
+
+       
+
         private ObservableCollection<int> _BaudRateList = new ObservableCollection<int>() { 9600,19200,38400,57600,115200,1200,2400,4800};
 
         public ObservableCollection<int> BaudRateList
@@ -120,6 +126,70 @@ namespace BaoYuanSerial.ViewModels
             set
             {
                 _FlowType = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        private string _ComPortState = "";
+
+        public string ComPortState
+        {
+            get => _ComPortState;
+            set
+            {
+                _ComPortState = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        private SolidColorBrush _ComPortStateColor = new SolidColorBrush(Colors.Red);
+        public SolidColorBrush ComPortStateColor
+        {
+            get => _ComPortStateColor;
+            set
+            {
+                _ComPortStateColor = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        private string _SendBytesStr = "Tx:0 Bytes";
+        public string SendBytesStr
+        {
+            get => _SendBytesStr;
+            set
+            {
+                _SendBytesStr = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        private SolidColorBrush _SendBytesColor = new SolidColorBrush(Colors.Green);
+        public SolidColorBrush SendBytesColor
+        {
+            get => _SendBytesColor;
+            set
+            {
+                _SendBytesColor = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        private string _ReceiveBytesStr="Rx:0 Bytes";
+        public string ReceiveBytesStr
+        {
+            get => _ReceiveBytesStr;
+            set
+            {
+                _ReceiveBytesStr = value;
+                this.RaisePropertyChanged();
+            }
+        }
+        private SolidColorBrush _ReceiveBytesColor = new SolidColorBrush(Colors.BlueViolet);
+        public SolidColorBrush ReceiveBytesColor
+        {
+            get => _ReceiveBytesColor;
+            set
+            {
+                _ReceiveBytesColor = value;
                 this.RaisePropertyChanged();
             }
         }
